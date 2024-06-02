ed101d283ffd650c3811abfd3d0fe9609b4165af
Amar <amarnath.arjsd@gmail.com>
2024-06-02 16:55:41 +0530

    llm_code_updated

---
+++ src/llm.py
@@ -0,0 +1,20 @@
+import os
+from dotenv import load_dotenv
+#from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+load_dotenv()
+
+GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
+os.environ['GOOGLE_API_KEY'] = GOOGLE_API_KEY
+
+def ask_bot(message):
+    llm=ChatGoogleGenerativeAI(model="gemini-pro")
+    response=llm.invoke(message)
+    return response.content
+
+if __name__=="__main__":
+    print('Hi!,Welcome to the chat bot!')
+    message=ask_bot("what is mean by llm?")
+    print(message)
+
